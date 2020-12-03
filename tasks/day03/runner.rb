430424f2182de53a5f35ab22dd9450ca07bdd2f3
Volodymyr Mykhailyk <712680+volodymyr-mykhailyk@users.noreply.github.com>
2020-12-03 10:06:10 +0200

    Day 3 part 2

--- tasks/day03/runner.rb
+++ tasks/day03/runner.rb
@@ -16,3 +16,20 @@ mapper = Navigation::TrajectoryMapper.new(map)
 trajectory_trees = mapper.calculate_trajectory(3, 1)
 
 info "Number of trees on [3,1] trajectory: #{trajectory_trees}"
+
+trajectories = [
+  [1, 1],
+  [3, 1],
+  [5, 1],
+  [7, 1],
+  [1, 2]
+]
+
+trees = trajectories.map do |trajectory|
+  trajectory_trees = mapper.calculate_trajectory(*trajectory)
+  info "Number of trees on #{trajectory.inspect} trajectory: #{trajectory_trees}"
+  trajectory_trees
+end
+
+info "Total number of trees: #{trees.reduce(&:*)}"
+
