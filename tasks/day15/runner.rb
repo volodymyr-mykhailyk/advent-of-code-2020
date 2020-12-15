5dd0ddba5bcc72060175282871017093fe631f24
Volodymyr Mykhailyk <712680+volodymyr-mykhailyk@users.noreply.github.com>
2020-12-15 21:23:37 +0200

    Day 15 part 2

--- tasks/day15/runner.rb
+++ tasks/day15/runner.rb
@@ -9,4 +9,8 @@ info "Playing memory game with: #{starting_numbers.inspect}"
 
 game = Entertainment::MemoryGame.new(starting_numbers)
 game.advance_to(2020)
-info "Last spoken number: #{game.current_number}"
+info "Last spoken number on turn 2020th: #{game.current_number}"
+
+game.advance_to(30000000)
+info "Last spoken number on turn 30000000th: #{game.current_number}"
+
